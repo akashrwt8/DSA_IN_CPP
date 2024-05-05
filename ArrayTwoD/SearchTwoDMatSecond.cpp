416a527125cf389475637706c9d566150b244318
akashrwt8 <akashsinghrawat80@gmail.com>
2024-05-05 14:53:13 +0530

    Added Search in two D matrix II code

--- ArrayTwoD/SearchTwoDMatSecond.cpp
+++ ArrayTwoD/SearchTwoDMatSecond.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int search2DMatTwo(int **arr, int n, int m, int key) {
+
+  int rowIndex = 0;
+  int colIndex = m - 1;
+  while (rowIndex < n && colIndex >= 0) {
+    int element = arr[rowIndex][colIndex];
+    if (element == key)
+    {
+      return 1;
+    } else if (element < key) {
+      rowIndex++;
+    } else {
+      colIndex++;
+    }
+  }
+  return 0;
+}
 
 void inputArray(int **arr, int n, int m) {
   for (int i = 0; i < n; i++) {
@@ -20,13 +37,25 @@ void printArray(int **arr, int n, int m) {
 }
 
 int main() {
-    int r, c, key;
+    int r, c, k;
     cout << "Enter row and column respectively : ";
     cin >> r >> c;
-    int *arr = new int*[r];
+    int **arr = new int*[r];
     for (int i = 0; i < r; i++)
     {
-        int *arr[i] = new int*[c];
-    }
+        arr[i] = new int[c];
+    } 
+  cout << " Enter element in array ";
+  inputArray(arr, r, c);
+  cout << "Enter key to search ";
+  cin >> k;
+  cout << "Array is " << endl;
+  printArray(arr, r, c);
+  int ans = search2DMatTwo(arr, r, c, k);
+  if (ans) {
+      cout << k << " is present in array";
+  } else {
+      cout << k << " is not present in array";
+  }
     
 }
